1b1eeda1c815ad39721b1fac2c36c75ecb053c7c
CamiloFernandez <160879374+CamiloFd@users.noreply.github.com>
2024-08-15 18:52:57 -0500

    main.java

--- main.java
+++ main.java
@@ -13,19 +13,19 @@ public static void main(String[] args) {
     if (edad < 2){
         System.out.println("Esta persona no podra viajar debido a que es menor de 2 años");
 
-    } if (edad >=2 && edad <5 ){
+    }else if (edad >=2 && edad <5 ){
         precio=precio-precio;
         System.out.println("El valor de su tiquete es de "+ precio);
 
-    } if(edad >=5 && edad <11){
+    }else if(edad >=5 && edad <11){
         precio=(precio/2);
         System.out.println("El valor de su tiquete es de "+ precio);
 
-    }if(edad >=11 && edad <15){
+    }else if(edad >=11 && edad <15){
         precio=(precio*0.75);
         System.out.println("El valor de su tiquete es de "+ precio);
 
-} if(edad>=15){
+}else if(edad>=15){
     System.out.println("El valor de su tiquete es de "+ precio);
 
 }
